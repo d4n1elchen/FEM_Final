dc5a7f6ee0ad1cd41f9c31bc1a2457b1880fda2b
Daniel Chen <team6612@gmail.com>
2017-01-15 00:40:46 +0800

    add figures

--- gaussElim.m
+++ gaussElim.m
@@ -7,14 +7,24 @@ function x=gaussElim(A)
     x = gpuArray(x);
   end
   fprintf('Gaus elimination\n');
+  textprogressbar('Gaus elimination: ');
+  tic;
   for i = 1:m-1
     a = -A(i+1:m,i)/A(i,i);
     A(i+1:m,:) = A(i+1:m,:) + a*A(i,:);
+    textprogressbar(i/(m-1)*100);
   end;
+  textprogressbar('done');
+  toc
 
   fprintf('Find unknown\n');
+  textprogressbar('Find unknown: ');
+  tic;
   x(m) = A(m,n)/A(m,m);
   for i = m-1:-1:1
     x(i) = (A(i,n) - A(i,i+1:m)*x(i+1:m))/A(i,i);
+    textprogressbar((m-i)/(m-1)*100);
   end
+  textprogressbar('done');
+  toc
 end
